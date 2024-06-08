4b7eab72a8c48da7a2a25c351112d77af64bd326
John-Sellers <sellers.e.john@gmail.com>
2024-06-08 10:17:03 -0400

    Change

--- learning-svelte/src/App.svelte
+++ learning-svelte/src/App.svelte
@@ -218,10 +218,10 @@
     It allows you to bind a value from a component to a property of an input element.
     When the value of the input changes, it updates the value of the component's property.
     When the property is updated in the component, the value of the input is updated as well. -->
-<script>
+<!-- <script>
     let value = 0;
 </script>
 
 <input bind:value type="number" />
 <div>value: {value}</div>
-<button on:click={() => (value = 0)}>Reset</button>
+<button on:click={() => (value = 0)}>Reset</button> -->
